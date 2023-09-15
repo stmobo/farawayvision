929694a84924c0fd8d9534d5f19160d553183177
FarawayVision <stmobo@gmail.com>
2023-09-15 05:42:14 -0500

    th08_8stages: minor fixes

--- repo.js
+++ repo.js
@@ -3,8 +3,8 @@
     "title": "FarawayVision's patches",
     "contact": "farawayvision (Discord)",
     "servers": [
-		"https://mirrors.thpatch.net/farawayvision/"
-	],
+        "https://mirrors.thpatch.net/farawayvision/"
+    ],
     "neighbors": [],
     "patches": {
         "th08_8stages": "(TH08) Allows playing stages 4A, 4B, 6A, and 6B all in a single run"
